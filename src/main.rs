f4653fb6cb38a63256125f3ee54a09f6513cc8ba
Max Johnson <maxdjohnson@gmail.com>
2024-12-07 16:08:00 -0800

    nix update

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,91 @@
-use nix::pty::{openpty, Winsize};
-use nix::unistd::{read, write};
-use nix::sys::socket::{
-    AddressFamily, SockAddr, UnixAddr, socket, connect, sendmsg, recvmsg,
-    ControlMessage, MsgFlags, SocketType,
-};
-use nix::sys::uio::IoVec;
 use nix::fcntl::{fcntl, FcntlArg, OFlag};
-use std::os::unix::io::RawFd;
+use nix::pty::openpty;
+use nix::sys::select::{select, FdSet};
+use nix::sys::socket::{
+    AddressFamily, SockaddrLike, UnixAddr, SockType, SockFlag, socket, connect,
+    sendmsg, recvmsg, ControlMessage, MsgFlags,
+};
+use nix::unistd::{close, read, write};
+use std::os::fd::{AsFd, BorrowedFd, RawFd};
 use std::process::exit;
+use std::io::{IoSlice, IoSliceMut};
 
 fn main() {
     let server_path = "/tmp/pyforked-server.sock";
 
-    // Ensure pyforked-server is running externally.
-
     // Allocate pty locally
     let pty = openpty(None, None).expect("openpty failed");
-    // pty.master and pty.slave are our FDs
-    // We'll send pty.slave to the server
+    let master_fd = pty.master;
+    let slave_fd = pty.slave;
 
-    let fd = socket(AddressFamily::Unix, SocketType::Stream, 0).unwrap();
-    let addr = UnixAddr::new(server_path).unwrap();
-    connect(fd, &SockAddr::Unix(addr)).unwrap();
+    // Create a Unix socket and connect
+    let fd = socket(AddressFamily::Unix, SockType::Stream, SockFlag::empty(), None)
+        .expect("socket failed");
+    let addr = UnixAddr::new(server_path).expect("UnixAddr failed");
+    connect(fd, &addr).expect("connect failed");
 
     // send pty.slave via SCM_RIGHTS
     {
-        let iov = [IoVec::from_slice(b"RUN")];
-        let cmsg = [ControlMessage::ScmRights(&[pty.slave])];
-        sendmsg(fd, &iov, &cmsg, MsgFlags::empty(), None).unwrap();
+        let iov = [IoSlice::new(b"RUN")];
+        let cmsg = [ControlMessage::ScmRights(&[slave_fd])];
+        sendmsg(BorrowedFd::borrow_raw(fd), &iov, &cmsg, MsgFlags::empty(), None)
+            .expect("sendmsg failed");
     }
 
     // Receive response from server
     let mut buf = [0u8; 1024];
-    let msg = recvmsg(fd, &mut [IoVec::from_mut_slice(&mut buf)], None, MsgFlags::empty()).unwrap();
+    let mut iov = [IoSliceMut::new(&mut buf)];
+    let msg = recvmsg(BorrowedFd::borrow_raw(fd), &mut iov, None, MsgFlags::empty())
+        .expect("recvmsg failed");
     let response = &buf[..msg.bytes];
     if response != b"OK" {
         eprintln!("Server did not respond with OK");
         exit(1);
     }
 
-    // We no longer need pty.slave fd
-    nix::unistd::close(pty.slave).unwrap();
+    // Close slave fd locally
+    close(slave_fd).expect("close slave failed");
 
-    // Now proxy data between our stdin/stdout and pty.master
+    // Now proxy data between stdin/stdout and pty.master
     set_nonblocking(0);
     set_nonblocking(1);
-    set_nonblocking(pty.master);
+    set_nonblocking(master_fd);
 
     let stdin_fd = 0;
     let stdout_fd = 1;
-    let master_fd = pty.master;
 
     let mut buf_in = [0u8; 1024];
     let mut buf_out = [0u8; 1024];
 
     loop {
-        let mut fds = nix::sys::select::FdSet::new();
-        fds.insert(stdin_fd);
-        fds.insert(master_fd);
+        let mut fds = FdSet::new();
+        fds.insert(BorrowedFd::borrow_raw(stdin_fd));
+        fds.insert(BorrowedFd::borrow_raw(master_fd));
         let nfds = std::cmp::max(stdin_fd, master_fd) + 1;
-        let res = nix::sys::select::select(
+        let res = select(
             nfds,
             Some(&mut fds),
             None,
             None,
             None
         );
-        if res.is_err() {
+        if let Err(_) = res {
             break;
         }
 
-        if fds.contains(stdin_fd) {
-            match read(stdin_fd, &mut buf_in) {
+        if fds.contains(BorrowedFd::borrow_raw(stdin_fd)) {
+            match read(BorrowedFd::borrow_raw(stdin_fd), &mut buf_in) {
                 Ok(n) if n > 0 => {
-                    let _ = write(master_fd, &buf_in[..n]);
+                    let _ = write(BorrowedFd::borrow_raw(master_fd), &buf_in[..n]);
                 }
                 _ => {}
             }
         }
 
-        if fds.contains(master_fd) {
-            match read(master_fd, &mut buf_out) {
+        if fds.contains(BorrowedFd::borrow_raw(master_fd)) {
+            match read(BorrowedFd::borrow_raw(master_fd), &mut buf_out) {
                 Ok(n) if n > 0 => {
-                    let _ = write(stdout_fd, &buf_out[..n]);
+                    let _ = write(BorrowedFd::borrow_raw(stdout_fd), &buf_out[..n]);
                 }
                 _ => {}
             }
